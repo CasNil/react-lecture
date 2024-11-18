2345f9ad9d7ac0a6fd5e54a33886c6d9892e7715
Casper Nilsson <caspernilsson13@gmail.com>
2024-11-18 15:17:04 +0100

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+
+
+const App = () =>{
+    return (
+    <>
+    <Navbar />
+    <Header />
+    </>
+    );
+};
+
+export default App;
