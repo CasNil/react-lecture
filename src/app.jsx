da3e7cbd69c3998954b43f4551e146012f88f69a
Casper Nilsson <caspernilsson13@gmail.com>
2024-11-18 16:39:25 +0100

    react lecture 2 (props)

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,38 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
+import AlertMessage from "./components/AlertMessage";
+import Card from "./components/Card";
+import { BsCheckCircleFill, BsFillXCircleFill } from "react-icons/bs";
+import Pricing from "./components/Pricing";
 
+const App = () => {
+  const apiResponse = {
+    status: 200,
+    message: "Valid Inputs",
+  };
 
-const App = () =>{
-    return (
+  const handleClick = (msg) => {
+    alert(msg);
+  };
+  return (
     <>
-    <Navbar />
-    <Header />
+      {apiResponse && apiResponse.status === 200 && (
+        <AlertMessage
+          icon={<BsCheckCircleFill />}
+          type="success"
+          message={apiResponse.message}
+        />
+      )}
+      {apiResponse && apiResponse.status === 400 && (
+        <AlertMessage
+          icon={< BsFillXCircleFill/>}
+          type="danger"
+          message={apiResponse.message}
+        />
+      )}
+
+    <Pricing />
     </>
-    );
+  );
 };
 
 export default App;
---
+++ src/components/AlertMessage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AlertMessage = (props) => {
+  return (
+  <div className={`alert alert-${props.type}`}>
+    {props.icon && <span>{props.icon}</span>}
+    {props.message}
+    </div>
+  );
+};
+
+export default AlertMessage;
